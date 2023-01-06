faf16970a6a5586b8c0cec28570dee7054a58897
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-01-06 11:56:36 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
+import time
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+#driver = webdriver.Chrome()
+driver.get("https://www.facebook.com/")
+
+time.sleep(4)
+driver.find_element_by_id("email").send_keys('9898989898') #replace with your email or phone  number
+time.sleep(2)
+driver.find_element_by_id("pass").send_keys('pass123$%') #replace with your password
+time.sleep(2)
+driver.find_element_by_name("login").click()
+time.sleep(4)
+driver.get('https://www.facebook.com/profile.php?id=100089108025261') #replace with account url or that account which you want to report it.
+time.sleep(4)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[2]/div/div/div/div[1]').click()#three dots
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]/div/div[1]/div/div[2]').click()#find or report
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[4]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[3]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div/div[1]/div/div/div/div/span').click()#report option
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[4]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[4]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div/div[1]/div/div/div/div/span').click()#me
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[4]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[4]/div/div/div[4]/div/div').click()#submit
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[4]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[2]/div[2]/div/div').click()#next button
+time.sleep(3)
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[4]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div[5]/div[2]/div/div/div/div').click() #done
+time.sleep(30)
+
